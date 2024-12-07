f23340993dbfd9e081e87de5f37e2825c2a7d9b0
Mohamed_Anwar <152428382+anwrrr@users.noreply.github.com>
2024-12-07 21:30:46 +0200

    new Require

--- Entities/Models/Course.cs
+++ Entities/Models/Course.cs
@@ -6,6 +6,7 @@ namespace Entities.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public string CourseLink { get; set; }
