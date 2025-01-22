6a844727d6663717e2bdc98c178c135c4906c7bb
Mohamed_Anwar <152428382+anwrrr@users.noreply.github.com>
2025-01-22 17:05:03 +0200

    add another class lib

--- Service.Contracts/obj/Debug/net8.0/Service.Contracts.AssemblyInfo.cs
+++ Service.Contracts/obj/Debug/net8.0/Service.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e874d6173ddf463f7b71ed55a27dfcf9e13256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb8fc73f741ccc58000965959c6dfe435b0293d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
