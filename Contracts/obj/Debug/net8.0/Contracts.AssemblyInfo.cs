a9f4eb258519cd5733b0f403f3e6bc483d176f98
Mohamed_Anwar <152428382+anwrrr@users.noreply.github.com>
2025-01-22 19:35:00 +0200

    Buliding arch

--- Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb8fc73f741ccc58000965959c6dfe435b0293d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a844727d6663717e2bdc98c178c135c4906c7bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
