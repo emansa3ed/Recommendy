e80055b94482facb97b2f4c8aa84018d871de687
Mohamed_Anwar <152428382+anwrrr@users.noreply.github.com>
2025-01-31 21:41:02 +0200

    enable photo in auth

--- Presentation/Controllers/AuthenticationController.cs
+++ Presentation/Controllers/AuthenticationController.cs
@@ -22,7 +22,7 @@ namespace Presentation.Controllers
 
         [HttpPost("Register")]
        
-        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration )
+        public async Task<IActionResult> RegisterUser([FromForm] UserForRegistrationDto userForRegistration )
         { 
             var result = await
            _service.AuthenticationService.RegisterUser(userForRegistration);
--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9a5103d06e036ef96189fe53710cf6f733e539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ff8ec369d5b55946bb38f1ce6ce247c4e9f961")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
